7debc6d135bb641211fef77ea9c0c87d8350dc74
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-06-11 16:56:40 -0400

    output filenames

--- deaths/national_deaths_metric.R
+++ deaths/national_deaths_metric.R
@@ -212,5 +212,5 @@ g2 <- ggplot(national_deaths_metric2) +
 
 gtot <- ggarrange(g1, g2, nrow=1, draw=FALSE)
 
-ggsave("national_death_metric.pdf", gtot, width=8, height=6)
-ggsave("national_death_metric.png", gtot, width=8, height=6)
+ggsave("national_deaths_metric.pdf", gtot, width=8, height=6)
+ggsave("national_deaths_metric.png", gtot, width=8, height=6)
