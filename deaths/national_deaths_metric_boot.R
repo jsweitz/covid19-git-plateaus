891843034e5eb35e6c8f8e274396a2028bd3089e
parksw3 <parksw3@mcmaster.ca>
2020-06-13 20:57:47 -0400

    clean up

--- deaths/national_deaths_metric_boot.R
+++ deaths/national_deaths_metric_boot.R
@@ -3,6 +3,7 @@ library(ggplot2); theme_set(theme_bw())
 library(lubridate)
 library(directlabels)
 library(egg)
+library(tikzDevice)
 
 cutoff <- 0.1
 
@@ -178,31 +179,42 @@ us_death_fit0a <- us_death_fit0 %>%
   mutate(
     pred2=pred-min(pred)+1,
     day2=(day-min(day))
-  )
+  ) %>%
+  merge(us_death_fit3)
 
 us_death_fit0a_end <- us_death_fit0a %>%
+  group_by(region) %>%
   filter(day2==max(day2))
 
 g1 <- ggplot(us_death_fit0a) +
-  geom_line(aes(day2, pred, group=region, col=region, lty=region)) +
-  geom_point(data=us_death_fit0a_end, aes(day2, pred, group=region, fill=region), shape=21, col=1) +
+  geom_line(aes(day2, pred, group=region, col=metric, lty=region)) +
+  geom_point(data=us_death_fit0a_end, aes(day2, pred, group=region, fill=metric), shape=21, col=1) +
+  geom_dl(aes(day2, pred, label=region, col=metric), method=list("last.bumpup", hjust=-0.2)) +
   scale_x_continuous("Time since $t_1$ (days)", expand=c(0, 0), limits=c(0, 78)) +
   scale_y_log10("Smoothed daily number of deaths") +
-  scale_color_manual(values=rep(c(1,2), 8)) +
-  scale_fill_manual(values=rep(c(1,2), 8)) +
-  scale_linetype_manual(values=c(1:9, 1:9))
-
-g1a <- direct.label(g1, list("last.bumpup", hjust=-0.2))
+  scale_color_gradientn(colors=c("black", "#8a0072", "#cf2661", "#f66d4e", "#ffb34a")) +
+  scale_fill_gradientn(colors=c("black", "#8a0072", "#cf2661", "#f66d4e", "#ffb34a")) +
+  scale_linetype_manual(values=c(1:9, 1:9)) +
+  theme(
+    legend.position = "none",
+    panel.grid = element_blank()
+  )
 
 g2 <- ggplot(us_death_fit3) +
-  geom_point(aes(metric, region)) +
-  geom_errorbarh(aes(xmin=lwr, xmax=upr, y=region), height=0) +
+  geom_point(aes(metric, region, col=metric)) +
+  geom_errorbarh(aes(xmin=lwr, xmax=upr, y=region, col=metric), height=0) +
+  scale_color_gradientn(colors=c("black", "#8a0072", "#cf2661", "#f66d4e", "#ffb34a")) +
   scale_x_continuous("Symmetry coefficient") +
   scale_y_discrete("States", limits = rev(us_death_fit3$region)) +
   theme(
-    legend.position = "none"
+    legend.position = "none",
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
   )
 
-gtot <- ggarrange(g1a, g2, nrow=1)
+gtot <- ggarrange(g1, g2, nrow=1)
 
-ggsave("national_death_metric2.pdf", gtot, width=12, height=6)
+tikz(file = "national_deaths_metric_boot.tex", width = 10, height = 5, standAlone = T)
+print(gtot)
+dev.off()
+tools::texi2dvi('national_deaths_metric_boot.tex', pdf = T, clean = T)
